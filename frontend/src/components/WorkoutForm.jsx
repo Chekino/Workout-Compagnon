88f2742902e214841ae2d3661de5c0139759c09f
Cheikh <diakite.cheikh200@gmail.com>
2024-08-02 21:18:47 +0200

    Frontend et Backend

---
+++ frontend/src/components/WorkoutForm.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+const WorkoutForm = () => {
+    const [title, setTitle] = useState("")
+    const [load, setLoad] = useState("")
+    const [reps, setReps] = useState("")
+    const [error, setError] = useState(null)
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault()
+
+        const workout = {title,load, reps}
+
+        const response = await fetch('/api/workouts', {
+            method:'POST',
+            body: JSON.stringify(workout),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const json = await response.json()
+
+        if(!response.ok){
+           setError(json.error)
+        }
+        if(response.ok){
+            setTitle('')
+            setLoad('')
+            setReps('')
+            setError(null)
+            console.log('new workot added', json)
+        }
+    }
+    return (
+        <form  className="create" onSubmit={handleSubmit}>
+            <h3>Ajouter nouveau Entrainement</h3>
+            <label htmlFor="">Titre d'Exerrcice:</label>
+            <input type="text" onChange={e => setTitle(e.target.value)} value={title} />
+            <label htmlFor="">Kg:</label>
+            <input type="number" onChange={e => setLoad(e.target.value)} value={load} />
+            <label htmlFor="">Nombre de répétition:</label>
+            <input type="number" onChange={e => setReps(e.target.value)} value={reps} />
+
+            <button>Ajouter l'Entrainement</button>
+            {error && <div className='error'>{error}</div>}
+        </form>
+       
+    );
+}
+
+export default WorkoutForm;
